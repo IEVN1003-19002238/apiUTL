199568502bab3d6791e39fa15467b9c42996beaa
Jorge Urbina <alb15.urbi@gmail.com>
2024-11-06 18:51:16 -0600

    commit final servicios terminados

--- src/config.py
+++ src/config.py
@@ -1,8 +1,8 @@
 class DevelopmentConfig:
     DEBUG = True
     MYSQL_HOST = '127.0.0.1'
-    MYSQL_USER = 'root'  # Asegúrate de agregar tu usuario de MySQL
-    MYSQL_PASSWORD = ''  # Cambia esto si tu contraseña es diferente
+    MYSQL_USER = 'root'  
+    MYSQL_PASSWORD = 'root'  
     MYSQL_DB = 'api_utl'
  
 config = {
